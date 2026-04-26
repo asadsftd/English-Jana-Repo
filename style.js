273569ea99a459657d26aa62dfd06638e73bee1e
asadsftd <asadkhanopu6@gmail.com>
2026-04-26 14:02:23 -0700

    added the my responsive sections commit bloge

--- style.js
+++ style.js
@@ -1,6 +1,18 @@
-document.getElementById("list-icon")
+document.getElementById("list-icon-two").style.display="none";
+
+
+
+document.getElementById("list-icon-three")
 .addEventListener("click", ()=>{
     console.log("Hellow Bangladesh");
     const navTwo = document.getElementById("nav-two");
     navTwo.style.display="block";
+    document.getElementById("list-icon-two").style.display="block";
+})
+document.getElementById("list-icon-two")
+.addEventListener("click", ()=>{
+    console.log("Asad");
+     const navTwo = document.getElementById("nav-two");
+    navTwo.style.display="none";
+    document.getElementById("list-icon-two").style.display="none";
 })
