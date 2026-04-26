f85e31dbb873c31f58f441fd31514a1bba34513d
asadsftd <asadkhanopu6@gmail.com>
2026-04-26 13:35:03 -0700

    added the my nav sections bloge commit

---
+++ style.js
@@ -0,0 +1,6 @@
+document.getElementById("list-icon")
+.addEventListener("click", ()=>{
+    console.log("Hellow Bangladesh");
+    const navTwo = document.getElementById("nav-two");
+    navTwo.style.display="block";
+})
