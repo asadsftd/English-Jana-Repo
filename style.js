30043fa8559fac92a268aff3e2ca629dff767837
asadsftd <asadkhanopu6@gmail.com>
2026-04-30 12:52:38 -0700

    added the my all sections js bloge commit

--- style.js
+++ style.js
@@ -14,3 +14,34 @@ document.getElementById("list-icon-two")
     document.getElementById("list-icon-one").style.display="block";
     document.getElementById("list-icon-two").style.display="none";
 })
+// 
+document.getElementById("nav").style.display="none";
+document.getElementById("btn")
+.addEventListener("click", (event)=>{
+    console.log("Hellow Opu");
+    const pass=document.getElementById("pass").value;
+    console.log(typeof pass);
+    const convertedPass=parseInt(pass);
+    console.log(convertedPass);
+    const inputName=document.getElementById("input-text").value;
+      if(convertedPass===123456){
+        document.getElementById("nav").style.display="block";
+    document.getElementById("nav").style.display="flex";
+    // banner
+    document.getElementById("banner-container").style.display="none";
+    }
+    else{
+        alert("Current Your Pin Number");
+    }
+
+    
+})
+
+// log Out
+document.getElementById("log-out")
+.addEventListener("click", (event)=>{
+    console.log("Hellow Adiba");
+    document.getElementById("nav").style.display="none";
+    document.getElementById("banner-container").style.display="block";
+    document.getElementById("banner-container").style.display="flex";
+})
